b090e2ad8af3be2278da93442854debd2564c6e3
Joel Kniaz <mygithubid@github.fake>
2013-05-23 09:23:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '0967f2b36e0b62c91ded3c6e64d037b70655f58a251e144e7fc6d76251a61c07b7d1609f01e8e38c209f935c96ca13d690425aa4ecd9c4f5b152b3e75d777cc9'
